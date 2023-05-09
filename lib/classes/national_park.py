cd12d74fc0e93374726c2e4a3385f6978b93d055
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-09 15:08:03 -0600

    confused

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -4,17 +4,32 @@ class NationalPark:
         self.name = name
         self._trips = []
         self._visitors = []
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and not hasattr(self, 'name'):
+            self._name = new_name
+        else:
+            raise Exception
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        if isinstance(new_visitor, Visitor):
+            self._visitors.append(new_visitor)
+        return self._visitors
     
     def total_visits(self):
         pass
     
     def best_visitor(self):
-        pass
+        pass 
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -2,10 +2,22 @@ class Visitor:
 
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 1 <= len(new_name) <= 15:
+            self._name = new_name
+        else:
+            raise Exception
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
         pass
+
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
